6492f004a51638f8a2ef31370273f3310cfdaccc
Mr.Robot <570776687@qq.com>
2018-02-02 10:21:29 +0800

    加了个最远参数
    投射物技能脚本为SkillCast
    技能的初始化方向写在skillController脚本的SkillTrigger函数里

--- Project/DustWar-Skill/Assets/Scripts/SkillCast.cs
+++ Project/DustWar-Skill/Assets/Scripts/SkillCast.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class SkillCast : MonoBehaviour
 {
     public float Velocity=3.0f;
+    public float MaxDistance;
+    private float curDistance;
 	void Start () 
     {
-
+        curDistance = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
       transform.Translate(transform.forward* Velocity * Time.deltaTime,Space.World);
+      if (curDistance < MaxDistance)
+      {
+          curDistance += Velocity * Time.deltaTime;
+      }
+      else
+      {
+          Destroy(gameObject);
+      }
 	}
     void OnTriggerEnter(Collider Col)
     {
